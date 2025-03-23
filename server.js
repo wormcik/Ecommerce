7537eea2beb3430a513d8db29e0ffc3b0a4ea4a3
Fatih KESİK <fatihkesik@usta.com.tr>
2025-03-24 00:27:24 +0300

    test

--- server.js
+++ server.js
@@ -1,19 +1,17 @@
 import express from 'express';
 import cors from 'cors';
-import { connectDB } from './connect.js';  // Correct import
-import routes from './routes.js';  // Assuming routes.js is in the same folder
+import { connectDB } from './connect.js';  
+import routes from './routes.js';  
 import 'dotenv/config';
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Enable CORS (Required for Vercel)
 app.use(cors({ origin: '*' }));
 app.use(express.json());
 app.use('/api', routes);
-app.use(express.static('public')); // ✅ Serve public folder
 
-// Connect to MongoDB before starting the server
+// MongoDB bağlantısını kur ve server'ı başlat
 (async () => {
   await connectDB();
   app.listen(port, () => {

2deca7aa5983892fb02cf74c2166de31a9a59a3b
Fatih KESİK <fatihkesik@usta.com.tr>
2025-03-24 18:35:54 +0300

    test

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Catch-all route to serve frontend
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.sendFile(path.join(__dirname, 'public', 'indexx.html'));
 });
 
 // Start the server

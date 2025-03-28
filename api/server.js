ea7f13b26f9825951c0826b5ec1c94f47b4dbda5
Fatih KESİK <fatihkesik@usta.com.tr>
2025-03-28 23:38:51 +0300

    test

--- api/server.js
+++ api/server.js
@@ -10,14 +10,16 @@ const app = express();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+// Önce statik dosyalar
+app.use(express.static(path.join(__dirname, '../public')));
+
+// API yollarını sonra tanımlayın
 app.use(cors());
 app.use(express.json());
 app.use(bodyParser.json());
-
 app.use('/api', router);
 
-app.use(express.static(path.join(__dirname, '../public')));
-
+// Herhangi bir yönlendirme için statik dosya
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../public', 'index.html'));
 });
